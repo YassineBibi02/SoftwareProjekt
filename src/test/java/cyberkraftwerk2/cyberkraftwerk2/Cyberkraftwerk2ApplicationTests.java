2ce4cc4b949b5e0b2a0b944d6c59d757da815550
Ski1l1ess <ski1l1ess@ski1l1ess-laptop>
2023-11-01 15:40:28 +0100

    java spring part 2

---
+++ src/test/java/cyberkraftwerk2/cyberkraftwerk2/Cyberkraftwerk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cyberkraftwerk2.cyberkraftwerk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cyberkraftwerk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
