016141eb215813cc1895c30767d9b31b43cd22c9
Yassine Bibi <Yassine.bibi@stud.uni-hannover.de>
2023-11-13 19:11:32 +0100

    Docker Funktioniert mit Postgres Server

--- src/test/java/cyberkraftwerk2/cyberkraftwerk2/Cyberkraftwerk2ApplicationTests.java
+++ src/test/java/cyberkraftwerk2/cyberkraftwerk2/Cyberkraftwerk2ApplicationTests.java
@@ -2,12 +2,3 @@ package cyberkraftwerk2.cyberkraftwerk2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Cyberkraftwerk2ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
