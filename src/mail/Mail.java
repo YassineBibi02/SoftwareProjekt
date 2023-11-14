2f6cd4f14c69f57a1ad70a80866632967907c1d6
Benjamin Moser <bmoser@gmx.de>
2023-11-14 16:56:12 +0100

    Mail and User class

--- src/mail/Mail.java
+++ src/mail/Mail.java
@@ -1,8 +1,10 @@
 public class Mail {
 
+    private static String MAILPATH = "path";
+
     /*save a new mail*/
     public void new_mail(String text, int level){
-
+        /*search for highest mail id*/
     }
 
     /*save a mail that has been edited*/
@@ -12,6 +14,10 @@ public class Mail {
 
     /*load a mail and return it to be displayed*/
     public String get_mail(int mail_id){
+        return null;
+    }
+
+    public int get_level(int mail_id){
         return null;
     }
     
