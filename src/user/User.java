a681d294867e6ea305c46ee2adcb38164045f7f2
Benjamin Moser <bmoser@gmx.de>
2023-11-14 17:29:23 +0100

    some code

--- src/user/User.java
+++ src/user/User.java
@@ -8,22 +8,41 @@ public class User {
 
     public int ID;
 
-    public String MailsReceived;
+    public int[] MailsReceived;
 
     public int MailLevel;
 
+    public int UserCount;
+
     /*loads the User with "ID" from Database */
     public User load_user(int ID){
-        return null;
+        User loaduser = new User();
+
+        /*load from database*/
+
+        return loaduser;
     }
     
     /*loads all Users from Database*/
     public User[] load_all(){
-        return null;
+        User[] Users = User[UserCount];
+        for(i = 1; i <= UserCount; i++){
+            Users[i] = this.load_user(i);
+        }
+        return Users;
     }
 
-    /*add new User to database, id set automatically*/
+    /*add new User to database, id set automatically, raise UserCount for all Users*/
     public void add_user(String firstname, String lastname, String email){
+        int defaultuser = 0;
+        int newID = this.load_user(defaultuser).ID++;
+        int[] receivedmails = null;
+        int newmaillevel = 1;
+        int UserCount = newID;
+
+        /*save user to database*/
+
+        /*update UserCount for all Users in Database*/
 
     }
 }
