2b9bdb21c7678b8ebf5e499fd76eed720ea7dd54
Yassine Bibi <Yassine.bibi@stud.uni-hannover.de>
2023-11-17 17:40:38 +0100

    Alles was an Spring security gehört gelöcht. Weil wir jetzt keycloak nutzen.

--- src/main/java/SWP/Cyberkraftwerk2/Databank/DbInit.java
+++ src/main/java/SWP/Cyberkraftwerk2/Databank/DbInit.java
@@ -2,7 +2,6 @@ package SWP.Cyberkraftwerk2.Databank;
 
 import SWP.Cyberkraftwerk2.Models.User;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -22,20 +21,15 @@ public class DbInit implements CommandLineRunner {
      * Data repository
      */
     private UserRepository userRepository;
-    /**
-     * Password Encoder
-     */
-    private PasswordEncoder passwordEncoder;
+
 
     /**
      * Injection through constructor of the Data repository and the password Encoder
      *
      * @param userRepository  The Data Repository
-     * @param passwordEncoder Password Encoder
      */
-    public DbInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DbInit(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     /**
@@ -49,9 +43,9 @@ public class DbInit implements CommandLineRunner {
         this.userRepository.deleteAll();
 
         //Create Users
-        User ADMIN   = new User("ADMIN",passwordEncoder.encode("ADMIN"),"ADMIN","ADMIN_ROLE");
-        User USER = new User("USER",passwordEncoder.encode("123"),"USER","");
-        User SUPERUSER = new User("SUPERUSER",passwordEncoder.encode("s123"),"SUPERUSER","SUPERUSER_ROLE");
+        User ADMIN = new User("ADMIN");
+        User USER = new User("USER");
+        User SUPERUSER = new User("SUPERUSER");
 
         List<User> userList = Arrays.asList(ADMIN,USER,SUPERUSER);
 
--- src/main/java/SWP/Cyberkraftwerk2/Models/User.java
+++ src/main/java/SWP/Cyberkraftwerk2/Models/User.java
@@ -2,10 +2,6 @@ package SWP.Cyberkraftwerk2.Models;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 
 /**
  * User Class to keep track of Roles and Authorities
@@ -28,38 +24,14 @@ public class User {
     @Column(nullable = false)
     private String username;
 
-    /**
-     *The encrypted password
-     */
-    @Column(nullable = false)
-    private String password;
 
-    /**
-     *An Integer to indicate if the user is active and allowed to log into the system
-     */
-    private int active;
-    /**
-     *a String of Roles that are assigned to this user.
-     */
-    private String roles = "";
-    /**
-     *a String of Permissions that are assigned to this user.
-     */
-    private String Permissions = "";
 
     /**
      * Constructor function for the User class
      * @param username The User's Username
-     * @param password The User's Encrypted Password
-     * @param roles The User's Roles
-     * @param permissions The User's Permissions
      */
-    public User(String username, String password, String roles, String permissions) {
+    public User(String username) {
         this.username = username;
-        this.password = password;
-        this.active = 1;
-        this.roles = roles;
-        this.Permissions = permissions;
     }
 
     /**
@@ -83,55 +55,7 @@ public class User {
         return username;
     }
 
-    /**
-     * @return Password as a String
-     */
-    public String getPassword() {
-        return password;
-    }
 
-    /**
-     * @return Active status as an Integer
-     */
-    public int getActive() {
-        return active;
-    }
-
-    /**
-     * @return Roles as a String
-     */
-    public String getRoles() {
-        return roles;
-    }
-
-    /**
-     * @return Permissions as a String
-     */
-    public String getPermissions() {
-        return Permissions;
-    }
-
-    /**
-     * @return A list of Roles assigned to this user
-     */
-    public List<String> getRoleList(){
-        if(!this.roles.isEmpty()){
-            return Arrays.asList(this.roles.split(","));
-        }
-        return new ArrayList<String>();
-
-    }
-
-    /**
-     * @return A list of Permissions assigned to this user
-     */
-    public List<String> getPermissionList(){
-        if(!this.Permissions.isEmpty()){
-            return Arrays.asList(this.Permissions.split(","));
-        }
-        return new ArrayList<String>();
-
-    }
 
 
 
