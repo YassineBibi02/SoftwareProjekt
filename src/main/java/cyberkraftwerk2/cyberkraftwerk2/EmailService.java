6e5d3935969121afbaba36e1c5b42ba5cde326ca
Tristan Slodowski <tristan.slodowski@stud.uni-hannover.de>
2023-11-19 16:01:42 +0100

    JavaDoc for EmailService

--- src/main/java/cyberkraftwerk2/cyberkraftwerk2/EmailService.java
+++ src/main/java/cyberkraftwerk2/cyberkraftwerk2/EmailService.java
@@ -9,12 +9,25 @@ import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
 
+/**
+ * Class accessing the JavaMail and JakartaMail packages to send Emails
+ * @author Tristan Slodowski
+ * @version 19.11.2023
+ */
 @Service
 public class EmailService {
     
+    // default JavaMailSender to manage sending SimpleMailMessages or MimeMessages
     @Autowired
     private JavaMailSender mailSender;  // uebernimmt die Einstellungen die in JMailSender für die Bean vorgenommen wurden?
 
+    /**
+     * Method to send simple txt.-like messages using Email
+     * @param target String of the email address of the recipient
+     * @param subject String of the subject the resulting Mail is supposed to have
+     * @param body String containing the general message of the Mail to be sent
+     * @author Tristan Slodowski
+     */
     // Senden von "simplen" txt.-ähnlichen Mails
     public void sendEmail(String target, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -25,6 +38,13 @@ public class EmailService {
         mailSender.send(message);
     }
 
+    /**
+     * Method to send simple txt.-like messages using Email to multiple recipients at once
+     * @param targets String array of multiple email addresses of the recipients
+     * @param subject String of the subject the resulting mails are supposed to have
+     * @param body String containing the general message of the Mails to be sent
+     * @author Tristan Slodowski
+     */
     // Senden von "simplen" txt.-ähnlichen Mails an mehrere Adressen gleichzeitig
     public void sendEmail(String[] targets, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -35,6 +55,14 @@ public class EmailService {
         mailSender.send(message);
     }
 
+    /**
+     * Method to send more sophisticated mail messages using HTML to form the body
+     * @param to String of the mail address of the recipient
+     * @param subject String of the subject the resulting mail are supposed to have
+     * @param html_body String written in HTML to form the general body of the mail message
+     * @throws MessagingException in case of failure if the sending fails
+     * @author Tristan Slodowski
+     */
     // Senden von Mails basierend auf HTML
     public void sendHtmlMessage(String to, String subject, String html_body) throws MessagingException {
         MimeMessage msg = mailSender.createMimeMessage();
@@ -43,7 +71,7 @@ public class EmailService {
         msg.setRecipients(MimeMessage.RecipientType.TO, to);
         msg.setSubject(subject);
 
-        msg.setContent(html_body, "text/html; charset=utf-8");
+        msg.setContent(html_body, "text/html; charset=utf-8");  // html-String als Inhalt der Message und dementsprechend den passenden Mime-Typ einstellen
 
         mailSender.send(msg);
     }
