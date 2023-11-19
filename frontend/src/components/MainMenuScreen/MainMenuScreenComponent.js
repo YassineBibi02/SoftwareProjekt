057905558f229c7b171486771f014f730e8a422c
LordBacki <102655974+LordBacki@users.noreply.github.com>
2023-11-19 23:57:26 +0100

    Improved mail button design & position

--- frontend/src/components/MainMenuScreen/MainMenuScreenComponent.js
+++ frontend/src/components/MainMenuScreen/MainMenuScreenComponent.js
@@ -7,14 +7,34 @@ import { useNavigate } from 'react-router-dom';
 const MainMenuScreenComponent = () => {
     const navigate = useNavigate();
 
+    const ButtonStyle = {
+        margin: '50px',
+        padding: '20px',
+        backgroundColor: 'red',
+        color: 'white',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: '50px'
+    };    
+
     const redirectToMail = () => {
         navigate('/mail');
     };
 
+    const Container = {
+        display: 'flex',
+        allignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '10px',
+        backgroundColor: 'white',
+    }
+
     return (
         <div>
             <Header/>
-            <button onClick={redirectToMail}>Go to Mail</button>
+            <div style = {Container}> 
+                <button style = {ButtonStyle} onClick={redirectToMail}>Go to Mail</button>
+            </div>
         </div>
     );
 };
