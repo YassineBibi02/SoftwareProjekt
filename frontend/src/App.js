bd5690c0fd4da6da252d1f8076e854784b7fa383
Danny <zwenger.danny@gmail.com>
2023-11-21 23:35:53 +0100

    Redirect zu loginserver

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import SendMailScreenComponent from './SendMailScreen/SendMailScreenComponent';
-import Home from './Home';
-import Header from './components/Header';
+import LoginScreen  from './pages/LoginScreen';
 import ReactDOM from 'react-dom';
 import MainMenuScreenComponent from './components/MainMenuScreen/MainMenuScreenComponent';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -13,10 +12,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element= { <MainMenuScreenComponent/> }/>
-        <Route path="/mail" element= { <SendMailScreenComponent/> }/>
-        <Route path="/Home" element= { <Home/> }/>
-        
+        <Route exact path="/" element= { <LoginScreen/> }/>
+        <Route path="/menu" element= { <MainMenuScreenComponent/> }/>
+        <Route path="/mail" element= { <SendMailScreenComponent/> }/>    
       </Routes>       
     </Router>
   );  
