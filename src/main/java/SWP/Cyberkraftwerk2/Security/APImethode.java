fb7ba7eb2aa7de3ab16e60b44c3fefb50aa4ab38
Tristan Slodowski <tristan.slodowski@stud.uni-hannover.de>
2023-12-01 20:42:43 +0100

    deleted unused import

--- src/main/java/SWP/Cyberkraftwerk2/Security/APImethode.java
+++ src/main/java/SWP/Cyberkraftwerk2/Security/APImethode.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
