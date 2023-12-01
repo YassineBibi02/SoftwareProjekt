a74d8c956fe4e67713d590b5f6ed2fa4e3659daf
Tristan Slodowski <tristan.slodowski@stud.uni-hannover.de>
2023-12-01 21:02:51 +0100

    added LessonControl to the API

--- src/main/java/SWP/Cyberkraftwerk2/Controller/APImethode.java
+++ src/main/java/SWP/Cyberkraftwerk2/Controller/APImethode.java
@@ -1,12 +1,17 @@
 package SWP.Cyberkraftwerk2.Controller;
 
 import SWP.Cyberkraftwerk2.Databank.UserRepository;
+import SWP.Cyberkraftwerk2.Lessons.LessonControl;
 import SWP.Cyberkraftwerk2.Mail.EmailService;
 import SWP.Cyberkraftwerk2.Module.User;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -75,6 +80,121 @@ public class APImethode {
         }
 
         return name;
+    }
+
+    /**
+     * Method accessible by the Frontend to be able to upload lesson pdfs into the resources directory of the server.
+     * @param file File to be uploaded on the server
+     * @param redirectAttributes
+     * @return boolean whether the operation was successful
+     * @author Tristan Slodowski
+     */
+    @PostMapping("/UploadLesson")
+    public boolean uploadLesson(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        if(file.isEmpty()) {
+            System.out.println("[uploadLesson] No file provided. Please try again.");
+            return false;
+        }
+
+        try {
+            Path dest_path = Paths.get("\\frontend\\src\\ressources" + file.getOriginalFilename());
+            file.transferTo(dest_path);
+            System.out.println("[uploadLesson] Upload successful.");
+            return true;
+        } catch (Exception e) {
+            System.out.println("[uploadLesson] Upload failed.");
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Method for the Frontend to register a newly uploaded lesson into the lesson registry
+     * Ideal Input: "{"name", difficulty_level, {IDs of the quizzes related to the lesson}, {IDs of the achievements related to the lesson}}"
+     * @param input String array containing the name (as String), difficulty level (as Integer), an array of the related quiz IDs (as Integer[]) and an array of the related achievement IDs (as Integer[])
+     * @return boolean whether the operation was successful
+     * @author Tristan Slodowski
+     */
+    @PostMapping("/RegisterLesson")                                         // TODO Auslesen der Argumente aus den Inputs für alle Lesson-Methoden sicherer machen
+    public boolean registerLesson(@RequestBody String[] input) {
+        String name = input[0];
+        int difficulty = Integer.parseInt(input[1]);
+
+        String[] incoming_quids = (input[2].substring(1, input[2].length() - 1)).split(", ");
+        int[] quids = new int[incoming_quids.length];
+        for(int i = 0; i < incoming_quids.length; i++) {
+            quids[i] = Integer.parseInt(incoming_quids[i]);
+        }
+        String[] incoming_achids = (input[3].substring(1, input[3].length() - 1)).split(", ");
+        int[] achids = new int[incoming_achids.length];
+        for(int i = 0; i < incoming_achids.length; i++) {
+            achids[i] = Integer.parseInt(incoming_achids[i]);
+        }
+        
+        return LessonControl.addLessonEntry(name, difficulty, quids, achids);
+    }
+
+    /**
+     * Method for the Frontend to delete the registration of a lesson from the registry.
+     * @param input String array with the name of the lesson to be deleted
+     * @return boolean whether the operation was successful
+     * @author Tristan Slodowski
+     */
+    @PostMapping("/RemoveFromRegistry")
+    public boolean removeFromRegistry(@RequestBody String[] input) {
+        String name = input[0];
+
+        return LessonControl.removeLessonEntry(name);
+    }
+
+    /**
+     * Method for the Frontend to change the main name of a lesson registration
+     * @param input String array containing the old and new name of and for a lesson; {old_name, new_name}
+     * @return boolean whether the operation was successful
+     * @author Tristan Slodowski
+     */
+    @PostMapping("/RenameInRegistry")
+    public boolean renameLessonInRegistry(@RequestBody String[] input) {
+        String old_name = input[0];
+        String new_name = input[1];
+
+        return LessonControl.updateLessonEntry(old_name, new_name);
+    }
+
+    /**
+     * Method for the Frontend to update the values of a lesson registration
+     * @param input String array containing the name (as String), difficulty level (as Integer), an array of the related quiz IDs (as Integer[]) and an array of the related achievement IDs (as Integer[])
+     * @return boolean whether the operation was successful
+     * @author Tristan Slodowski
+     */
+    @PostMapping("/UpdateInRegistry")
+    public boolean updateLessonInRegistry(@RequestBody String[] input) {
+        String name = input[0];
+        int difficulty = Integer.parseInt(input[1]);
+
+        String[] incoming_quids = (input[2].substring(1, input[2].length() - 1)).split(", ");
+        int[] quids = new int[incoming_quids.length];
+        for(int i = 0; i < incoming_quids.length; i++) {
+            quids[i] = Integer.parseInt(incoming_quids[i]);
+        }
+        String[] incoming_achids = (input[3].substring(1, input[3].length() - 1)).split(", ");
+        int[] achids = new int[incoming_achids.length];
+        for(int i = 0; i < incoming_achids.length; i++) {
+            achids[i] = Integer.parseInt(incoming_achids[i]);
+        }
+        
+        return LessonControl.updateLessonEntry(name, difficulty, quids, achids);
+    }
+
+    /**
+     * Method for the Frontend to get a json string representation of the lesson registry.
+     * Can be used to access all information about the registered lessons.
+     * @return Json-formatted String representation of the lesson registry
+     * @author Tristan Slodowski
+     */
+    @GetMapping("/GetLessonRegistry")
+    public String getJsonString() {
+        return LessonControl.getJsonString();
     }
 
     @GetMapping("/test1234")
