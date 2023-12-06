c2c834dc49c7711cf9b89638c2ecaf3808246638
Danny <zwenger.danny@gmail.com>
2023-12-06 14:26:45 +0100

    Header überarbeitet

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,44 @@
 import React , {useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginContext from '../globals/globalContext';
+import LogoImage from '../images/logo_schwarz.png';
 
 const Header = () => {
     const {isLoggedIn, setLoggedIn,userV} = useContext(LoginContext);
     const navigate = useNavigate();
 
     const HeaderStyle = {
-        margin: '20px',
+        //margin: '20px',
         padding: '20px',
         display : 'flex',
-        backgroundColor: '#ec6608',
-        color: 'white',
-        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        color: 'black',
         alignItems: 'center',
-        height: '80px',
-        borderRadius: '10px'
+        borderBottom: '2px solid #ec6608',
     };
 
-    const left = {
-        marginRight: 'auto',
-        fontSize: '2.5em'
+    const LogoContainer = {
+        maxWidth: '100%',
+        height: '100px',
+        marginRight: '50px'
     }
 
-    const right = {
-        marginLeft: '50px',
-        fontSize: '1.5em'
+    const Button = {
+        fontSize: '2.5em',
+        color: 'black'
+    }
+
+    const ButtonContainer = {
+        display: 'flex',
+        justifyContent: 'right',
+        alignItems: 'center',
+        color: 'black',
+        marginTop: 'auto',
+        backgroundColor: '#ec6608',
+        border: '1px solid #ec6608',
+        borderRadius: '10px',
+        width: '100%',
+        height: '100px'
     }
 
     const goToHome = () => {
@@ -38,19 +51,19 @@ const Header = () => {
 
     return (
         <div style={HeaderStyle}>
-             <p style={left} onClick={goToHome} role="button" tabIndex={0}>
-                            Cyber-Kraftwerk
-             </p>
-            <p style={right}>Einstellungen</p>
-            {
+            <img src={LogoImage} style={LogoContainer} onClick={goToHome} role='button'></img>
+            <div style={ButtonContainer}>
+                <p style = {{...Button, marginRight:'100px'}}>Einstellungen</p>
+                {
                     isLoggedIn() ?
                     <>
-                        <p style={right}>Welcome {userV.given_name}</p>
-                        <p style={right} onClick={login} >Logout</p>
+                        <p style={Button}>Welcome {userV.given_name}</p>
+                        <p style = {{...Button, marginRight:'50px'}} onClick={login} >Logout</p>
                     </>
                     :
-                    <p style={right} onClick={login} >Login</p>
-            }
+                    <p style = {{...Button, marginRight:'50px'}} onClick={login} >Login</p>
+                }
+            </div>
         </div>
     );
 };
