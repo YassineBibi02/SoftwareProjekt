d3a8cbc553c9bd9d2c8254aa6d33f80b0580bde6
Danny <zwenger.danny@gmail.com>
2023-12-13 14:11:35 +0100

    Header überarbeitet

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,36 +1,43 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginContext from '../globals/globalContext';
-import logo from '../images/Logo.png'; // Adjust the path as needed
+import logo from '../images/logo_weiß.png'; // Adjust the path as needed
 import userIcon from '../images/user.png'; // Adjust the path as needed
+import logoutIcon from '../images/logout_icon.png';
+import settingsIcon from '../images/settings_icon.png';
+import { Container } from 'react-bootstrap';
 
 
 
 const Header = () => {
     const { isLoggedIn, setLoggedIn, userV , login, logout} = useContext(LoginContext);
     const navigate = useNavigate();
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
 
     const HeaderStyle = {
         margin: '20px',
-        padding: '20px',
+        marginTop: '10px',
+        padding: '10px',
         display: 'flex',
-        backgroundColor: 'lightgrey', // Changed to light grey
-        justifyContent: 'space-between',
+        backgroundColor: '#333333', // Changed to light grey
         alignItems: 'center',
-        height: '80px',
         borderRadius: '10px',
-
+        border: '1px solid black',
     };
 
+    const LogoStyle = {
+        height: '50px',
+        cursor: 'pointer'
+    }
+
     const homeTextStyle = {
+        /*
         marginLeft: '40px', // Closer to the logo
         marginTop: '40px', // Closer to the top
         color: 'black', // Changed text color to black
         cursor: 'pointer',
         fontWeight: 'bold',
         fontSize: '1.7em' // Larger text size for visibility
+        */     
     };
 
     const LogoutStyle = {
@@ -41,93 +48,50 @@ const Header = () => {
             cursor: 'pointer',
             fontWeight: 'bold',
             fontSize: '1.7em' // Larger text size for visibility
-        };
+        };   
 
-    const logoStyle = {
-            height: '50px',
-            cursor: 'pointer'
-    };
+    const IconStyle = {
+        maxWidth: '50px',
+        maxHeight: '50px',
+        cursor: 'pointer',
+        marginLeft: '30px'
+    }
 
-    const userInfoBoxStyle = {
-            display: 'flex',
-            alignItems: 'center',
-            position: 'absolute',
-            right: '40px',
-            top: '26px',
-            backgroundColor: 'white',
-            border: '2px solid orange',
-            padding: '5px 10px', // Increase padding for a longer box
-            borderRadius: '5px',
-            cursor: 'pointer',
-            width: '250px', // You can set a specific width if you need to
-
-    };
-
-    const userDetailsStyle = {
+    const RightContainer = {
         display: 'flex',
-        flexDirection: 'column', // Stack children vertically
-        alignItems: 'flex-start', // Align children to the start of the flex container
-        marginRight: '10px', // Add some space to the right of the container
-        fontSize: '1.1em' ,// Decrease font size for visibility
-        fontWeight: 'bold' // Make the text bold
-    };
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
 
-    const dropdownMenuStyle = {
-            display: dropdownOpen ? 'block' : 'none',
-            position: 'absolute',
-            backgroundColor: 'white',
-            border: '2px solid orange',
-            right: '40px',
-            marginTop: '10px',
-            borderRadius: '5px',
-            zIndex: '1000',
-            width: '250px',
-            top: '83px'
-    };
-
-    const dropdownItemStyle = {
-            padding: '10px',
-            borderTop: '1px solid orange',
-            color: 'black'
-    };
+    const IconContainer = {
+        marginLeft: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+    }
 
     const goToHome = () => {
             navigate('/');
     };
 
-
-    const toggleDropdown = () => {
-            setDropdownOpen(!dropdownOpen);
-    };
-
     return (
-        <div style={HeaderStyle}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src={logo} style={{ height: '50px', cursor: 'pointer' }} onClick={goToHome} alt="logo"/>
-                <p style={homeTextStyle} onClick={goToHome}>HOME</p>
-            </div>
-            <div>
-                {isLoggedIn() ? (
-                    <div style={userInfoBoxStyle} onClick={toggleDropdown}>
-                        <img src={userIcon} alt="User Icon" style={{ marginRight: '20px', marginLeft: '20px' }}/>
-                        <div style={userDetailsStyle}>
-                            <p style={{ margin: 0, color: 'black', fontWeight: 'bold' }}>{userV.given_name}</p>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <span style={{ color: 'black', marginRight: '5px' }}>Level:</span>
-                                <p style={{ margin: 0, color: 'orange', fontWeight: 'bold' }}>{userV.mailLevel}</p>
-                            </div>
-                        </div>
-                        <p style={{ marginLeft: '40px', marginTop:"15px" }}>▼</p>
+        <div fluid style={HeaderStyle}>
+            <Container>
+                <img src={logo} style={LogoStyle} onClick={goToHome} alt="logo"/>
+            </Container>
+            
+            {isLoggedIn() ? (
+                <Container style={RightContainer}>
+                    <div style={IconContainer}>
+                        <h1 className='mt-2'style={{fontSize: '2em', color: 'white'}}>Level: {userV.mailLevel}</h1>
+                        <img src={userIcon} style={{...IconStyle, marginLeft:'50px'}}/>
+                        <img src={settingsIcon} style={IconStyle}/>
+                        <img src={logoutIcon} style={IconStyle} onClick={logout}/>
                     </div>
-                ) : (
-                    <p onClick={login} style={LogoutStyle}>Login</p>
-                )}
-                <div style={dropdownMenuStyle}>
-                    <div style={dropdownItemStyle} onClick={goToHome}>Einstellungen</div>
-                    <div style={dropdownItemStyle} onClick={goToHome}>Details/etc</div>
-                    <div style={dropdownItemStyle} onClick={logout}>Logout</div>
-                </div>
-            </div>
+                </Container>
+            ) : (
+                <p onClick={login} style={LogoutStyle}>Login</p>
+            )}
+                   
         </div>
     );
 };
