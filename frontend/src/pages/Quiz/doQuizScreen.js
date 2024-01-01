299fc3ca5c37c6adf9ac2e54926a184d46a596df
Danny <zwenger.danny@gmail.com>
2024-01-01 16:34:49 +0100

    Quizes now get correctly loaded from database

--- frontend/src/pages/Quiz/doQuizScreen.js
+++ frontend/src/pages/Quiz/doQuizScreen.js
@@ -12,26 +12,30 @@ function DoQuizScreen() {
 
     var lessonID = queryParameter.get("id");
     const [userData, setUserData] = useState([]);
-    const lesson = getLesson();
+    const [lessonData, setLessonData] = useState([]);
+    const quizData = lessonData.quiz;
 
-    async function getLesson() {
-      var lessonData = "";
+    // Gets the data of the lesson associated to the quiz and stores it in "lessonData"
+    useEffect(() => {
       try {
-          const response = await fetch('/api/methode/GetLessonRegistry', {
-              method: 'GET', credentials: 'include',
-              headers: {
-                  'X-XSRF-TOKEN': cookies['XSRF-TOKEN'],
-              },
-          }).then(response => response.text()).then(body => {lessonData = JSON.parse(body)});
-          console.log("Success:", lessonData);
-          return lessonData;
+        fetch('/api/methode/GetLessonRegistry', {
+          method: 'GET', 
+          credentials: 'include',
+          headers: {
+            'X-XSRF-TOKEN': cookies['XSRF-TOKEN'],
+            'Content-Type': 'application/json',
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          setLessonData(data[lessonID]);  // ToDO: Vielleicht sicherer zuerst nach der id zu überprüfen
+        });
       } catch (error) {
-          console.error("Error2:", error);
-          console.error("Error details:", error.message, error.response);
-          return null;
+        console.error('Error:', error);
       }
-    }
+    }, [])
     
+    // Gets the data of the current user and stores it in "userData"
     useEffect(() => {
       fetch('api/user', { credentials: 'include' }) // <.>
           .then(response => response.text())
@@ -44,26 +48,35 @@ function DoQuizScreen() {
           });
 
     }, []);
+    
+    // Fills the global questions variable with a all questions of the quiz
+    function fillQuestions() {
+      var element;
+      var options = [];
+      var result = {};
 
-    const questions = [
-      {
-        question: "Was ist die richtige Antwort?",
-        options: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4", "Antwort 5"],
-      },
-      {
-        question: "Was ist die richtige Antwort?",
-        options: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4", "Antwort 5"],
-      },
-      {
-        question: "Was ist die richtige Antwort?",
-        options: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4", "Antwort 5"],
-      },
-      {
-        question: "Was ist die richtige Antwort?",
-        options: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4", "Antwort 5"],
-      },
-      
-    ];
+      for (var i = 0; i < quizData.question_count; i++) {
+        var name = "q" + i;
+        element = quizData[name];
+        options.push(element.right_answer);
+        for (var j = 0; j < element.wrong_answers.length; j++) {
+          options.push(element.wrong_answers[j]);
+        }
+        // ToDo: Randomise the option order
+        result = {question: element.question, options: options}
+        questions.push(result);
+        options = [];
+        result = {};
+      }
+    }
+    
+    // Array with all questions of the quiz
+    let questions = [];
+
+    // Fill the variable only when the quiz data is loaded
+    if (quizData) {
+      fillQuestions();
+    }
     
     const [currentQuestion, setCurrentQuestion] = useState(0);  // The current question and the function to update it, first initialised with 0
     const [answers, setAnswers] = useState(Array(questions.length).fill(''));   // Array with all submitted answers, first initialised as empty
@@ -88,7 +101,8 @@ function DoQuizScreen() {
       newAnswers[currentQuestion] = event.target.value;
       setAnswers(newAnswers);
     };
-  
+    
+    // Sends the submitted answers to the backend
     function handleSubmit() {
       var submit = [];
       submit.push(lessonID);
@@ -135,37 +149,38 @@ function DoQuizScreen() {
     const CardStyle = {
         maxWidth: "800px"
     }
-  
+    
     return (
       <div>
         <Header/>
-        <h1 className="mt-5 d-flex justify-content-center">Quiz zur Schulung xy</h1>
+        <h1 className="mt-5 d-flex justify-content-center">Quiz zur Schulung "{lessonData.name}"</h1>
         <Card className="mx-auto mt-5" style={CardStyle}>
           <Card.Body>
             <Form>
               <Form.Group controlId="formQuestion">
                 <Form.Label style={QuestionStyle}>
-                    {questions[currentQuestion].question}
+                    {lessonData && quizData && questions[currentQuestion].question}
                 </Form.Label>
-                {questions[currentQuestion].options.map((option, index) => (
-                <Form.Check
-                    className='mt-3'
-                    style={AnswerStyle}
-                    key={index}
-                    type="radio"
-                    label={option}
-                    name="answer"
-                    value={option}
-                    checked={answers[currentQuestion] === option}
-                    onChange={handleAnswerChange}
-                />
-                ))}
+                  {lessonData && quizData &&
+                    questions[currentQuestion].options.map((option, index) => (
+                      <Form.Check
+                          className='mt-3'
+                          style={AnswerStyle}
+                          key={index}
+                          type="radio"
+                          label={option}
+                          name="answer"
+                          value={option}
+                          checked={answers[currentQuestion] === option}
+                          onChange={handleAnswerChange}
+                      />
+                    ))
+                  }
               </Form.Group>
             </Form>
           </Card.Body>
         </Card>
-  
-        <div className="mt-3 d-flex justify-content-center"> {/*mt-3 steht für margin-top mit value 3*/}
+        <div className="mt-3 d-flex justify-content-center">
           <Button variant="secondary" onClick={handlePrevQuestion} disabled={currentQuestion === 0}>
             Vorherige Frage
           </Button>
@@ -174,18 +189,17 @@ function DoQuizScreen() {
           </Button>
             {currentQuestion === questions.length - 1 && (
                 <Button variant="success" className="ms-2" onClick={handleSubmit}>
-                Bestätigen
+                  Bestätigen
                 </Button>
             )}
-        </div>
-  
+        </div> 
         <div className="mt-3 d-flex justify-content-center" style={{fontSize:'2em'}}>
           <p>
             Frage {currentQuestion + 1} von {questions.length}
           </p>
         </div>
       </div>
-    );
+    ); 
 }
   
 export default DoQuizScreen;
