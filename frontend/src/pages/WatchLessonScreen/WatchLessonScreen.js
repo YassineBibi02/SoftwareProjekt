8660679b4f92df361975184ada9f13ebe3583e10
Tristan Slodowski <tristan.slodowski@stud.uni-hannover.de>
2024-01-05 18:51:47 +0100

    implemented a better pdf viewer

--- frontend/src/pages/WatchLessonScreen/WatchLessonScreen.js
+++ frontend/src/pages/WatchLessonScreen/WatchLessonScreen.js
@@ -3,8 +3,11 @@ import Header from '../../components/Header';
 import { Button } from 'react-bootstrap';
 import { Route, Link, useParams, useLocation } from 'react-router-dom';
 import testPDF from '../../ressources/test.pdf';
-import pdfjs from 'pdfjs-dist/build/pdf';
-//import testPDF2 from '../../ressources/test_horizontal.pdf';
+import { Document, Page, pdfjs } from 'react-pdf'
+import testPDF2 from '../../ressources/test_horizontal.pdf';
+// style sheets for the pdf viewer; needs to be imported even if unused
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 import '../../globals/globals';
 
@@ -21,9 +24,24 @@ const WatchLessonScreen = () => {
         console.log("Path: " , path)
     }
 
+    // importing the worker from a reliable source
+    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
-    const [currentPage, setCurrentPage] = useState(1);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [numPages, setNumPages] = useState(null);
     const totalPages = 112; // Replace with the actual total number of pages in the PDF
+
+    // set the right page numbers and max page numbers when they are loaded
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    // method to change the pages a bit more easier
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
 
     const styles = {
         container: {
@@ -49,43 +67,47 @@ const WatchLessonScreen = () => {
             width: '100px',
             margin: '0 15px',
         },
+        TextLayer: {
+            visibility: "hidden",
+        },
+        AnnotationLayer: {
+            visibilty: "hidden",
+        },
     };
 
+    // roll the pdf pages back
     const handleBack = () => {
-        if (currentPage > 1) {
-            setCurrentPage(prevPage => prevPage - 1);
+        console.log("Pressed Backward")
+        if (pageNumber > 1) {
+            changePage(-1);
         }
     };
 
+    // advance the pdf pages forth
     const handleForward = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(prevPage => prevPage + 1);
+        console.log("Pressed Forward")
+        if (pageNumber < totalPages) {
+            changePage(1);
         }
     };
-
-    useEffect(() => {
-        const iframe = document.getElementById('pdfViewer');
-        iframe.onload = () => {
-            console.log('iframe loaded');
-            console.log(iframe.contentWindow);
-        };
-    }, [currentPage]);
-
 
     return (
         <div>
             <Header />
             <div style={styles.container}>
                 <h1 style={styles.lessonTitle}>{title}</h1>
-                <iframe
-                    id="pdfViewer"
-                    title="PDF Viewer"
-                    src={`${path}#page=${currentPage}`}
-                    style={styles.pdfViewer}
-                />
+                <Document 
+                    file={"../../..".concat("", path)} 
+                    onLoadSuccess={onDocumentLoadSuccess} 
+                >
+                    <Page pageNumber={pageNumber} />
+                </Document>
+                <p>
+                    Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+                </p>
                 <div style={styles.buttonContainer}>
-                    <Button style={styles.button} onClick={handleBack}>Zurück</Button>
-                    <Button style={styles.button} onClick={handleForward}>Vorwärts</Button>
+                    <Button style={styles.button} disabled={pageNumber <= 1} onClick={handleBack}>Zurück</Button>
+                    <Button style={styles.button} disabled={pageNumber >= numPages} onClick={handleForward}>Vorwärts</Button>
                 </div>
             </div>
         </div>
