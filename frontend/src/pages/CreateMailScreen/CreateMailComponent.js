7de2000f37dd5155e8f9bfe3183b1ebcad605c16
Danny <zwenger.danny@gmail.com>
2024-01-05 15:29:50 +0100

    Added legend for text when creating a template and added comments for code

--- frontend/src/pages/CreateMailScreen/CreateMailComponent.js
+++ frontend/src/pages/CreateMailScreen/CreateMailComponent.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Header from '../../components/Header';
-import { Form, Button, Container} from 'react-bootstrap';
+import { Form, Button, Container, Modal} from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
 
 function CreateMailComponent() {
+    const [cookies] = useCookies(['XSRF-TOKEN']);
 
     const [subject, setSubject] = useState(''); // The subject of the mail with its update function
     const [text, setText] = useState('');   // The text body of the mail with its update function
     const [level, setLevel] = useState(''); // The level of the mail with its update function
 
-    const mail = [];    // The mail, initially empty
+    const [showPopup, setShowPopup] = useState(false); // Sets if the popup is currently shown or not
 
-    const [cookies] = useCookies(['XSRF-TOKEN']);
+    const mail = [];    // The mail, initially empty
 
     // The style of a text field
     const FormGroupStyle = {
@@ -23,10 +24,16 @@ function CreateMailComponent() {
         fontSize: '2em'
     }
     
-    // The style for a button
-    const ButtonStyle = {
+    // The style for the submit button
+    const SubmitButtonStyle = {
         backgroundColor: '#ec6608',
         borderColor: '#ec6608'
+    }
+
+    // The style for the legend button
+    const LegendButtonStyle = {
+        backgroundColor: '#555555',
+        borderColor: '#555555'
     }
     
     // Updates the value of a variable when its changed
@@ -74,23 +81,35 @@ function CreateMailComponent() {
         mail.push(subject);
         mail.push(level);
     }
-    
+
+    function openPopup() {
+        setShowPopup(true);
+    }
+
+    function closePopup() {
+        setShowPopup(false);
+    }
 
     return (
         <div>
             <Header/>
             <Container>
+
+                {/* The formular */}
                 <Form>
+
+                    {/* Input field for subject */}
                     <Form.Group className="mb-3" controlId="SubjectInput">
                         <Form.Label style={FormLabelStyle}>Betreff</Form.Label>
                         <Form.Control
-                            placeholder="Betreff"
                             style={FormGroupStyle} 
                             type="subject"
                             name="Subject"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
+
+                    {/* Input field for text */}
                     <Form.Group className="mb-3" controlId="TextInput">
                         <Form.Label style={FormLabelStyle}>Textbereich</Form.Label>
                         <Form.Control 
@@ -101,6 +120,36 @@ function CreateMailComponent() {
                             onChange={handleInputChange} 
                         />
                     </Form.Group>
+
+                    {/* Button for the legend popup */}
+                    <Button
+                        style={LegendButtonStyle}
+                        onClick={openPopup}
+                    >
+                        Legende
+                    </Button>
+
+                    {/* Popup for the legend */}
+                    <Modal show={showPopup} onHide={closePopup} backdrop="static">
+                        <Modal.Header closeButton>
+                            Beim Erstellen von Mails stehen folgende Variablen zur Verfügung, die vom System automatisch ersetzt werden:
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>-LINK (muss in jeder Mail enthalten sein)</p>
+                            <p>-EMPFAENGERVORNAME (Vorname des Empfängers)</p>
+                            <p>-EMPFAENGERNACHNAME (Nachname des Empfängers)</p>
+                            <p>-EMPFAENGEREMAIL (E-Mail-Adresse des Empfängers)</p>
+                            <p>-KOLLEGE1VORNAME (Vorname zufälliger Kollege 1)</p>
+                            <p>-KOLLEGE1NACHNAME (Nachname zufälliger Kollege 1)</p>
+                            <p>-KOLLEGE1EMAIL (E-Mail-Adresse zufälliger Kollege 1)</p>
+                            <p>-KOLLEGE2VORNAME (Vorname zufälliger Kollege 2)</p>
+                            <p>-KOLLEGE2NACHNAME (Nachname zufälliger Kollege 2)</p>
+                            <p>-KOLLEGE2EMAIL (E-Mail-Adresse zufälliger Kollege 2)</p>
+                            <p>-COMPANY (Firmenname)</p>
+                        </Modal.Body>
+                    </Modal>
+                    
+                    {/* Checkboxes for levels */}
                     <Form.Group className="mb-3" controlId="LevelInput">
                         <Form.Label style={FormLabelStyle}>Level</Form.Label>
                         {['radio'].map((type) => (
@@ -135,9 +184,12 @@ function CreateMailComponent() {
                             </div>
                         ))}
                     </Form.Group>
-                <Button style={ButtonStyle} className="mt-3" onClick={handleSubmit}>Hinzufügen</Button>
+
+                    {/* Submit button */}
+                    <div className="d-flex justify-content-center">
+                        <Button style={SubmitButtonStyle} className="mt-3 btn-lg" onClick={handleSubmit} disabled={!subject || !text || !level}>Hinzufügen</Button>
+                    </div>
                 </Form>
-                
             </Container>
         </div>
       );
